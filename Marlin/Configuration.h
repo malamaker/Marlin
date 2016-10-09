bb0071180b576512b21815a943183c4c35c488cb
Joshua Malamaker <malamaker86@gmail.com>
2016-10-09 13:05:38 -0500

    Found out the induction sensor position help was inversed from what was actually happening

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -609,7 +609,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
   //    |           |
   //  L |    (+) P  | R <-- probe (20,20)
   //  E |           | I
-  //  F | (-) N (+) | G <-- nozzle (10,10)
+  //  F | (+) N (-) | G <-- nozzle (10,10)
   //  T |           | H
   //    |    (-)    | T
   //    |           |
@@ -617,7 +617,7 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
   //  (0,0)
   #define X_PROBE_OFFSET_FROM_EXTRUDER -26  // X offset: -left  +right  [of the nozzle]
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER 2   // Z offset: -below +above  [the nozzle]
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -2   // Z offset: -below +above  [the nozzle]
 
   #define XY_TRAVEL_SPEED 6000         // X and Y axis travel speed between probes, in mm/min.
 
